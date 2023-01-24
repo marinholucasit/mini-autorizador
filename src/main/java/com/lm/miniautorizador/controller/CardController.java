f6b32582e21a9618959404ccbd8eab2b1f33a3f5
Lucas Gomes <marinholucasit@gmail.com>
2023-01-24 12:07:15 -0300

    add docker-compose; add swagger; add CardController class; add actuatorHealth

---
+++ src/main/java/com/lm/miniautorizador/controller/CardController.java
@@ -0,0 +1,13 @@
+package com.lm.miniautorizador.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cartoes")
+@Tag(name = "Cartões", description = "Controller Cartões")
+public class CardController {
+
+
+}
